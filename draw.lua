5b52a2ef9efde52e85beb713afc2dfbc9c733cda
Paul <liquidream@hotmail.co.uk>
2019-12-07 17:19:58 +0000

    WIP

--- draw.lua
+++ draw.lua
@@ -3,7 +3,7 @@
 -- draw the actual game 
 -- (including the title screen)
 function draw_game()
-  cls() --5
+  cls(2) --5
 
   camera(0,0)
   
@@ -15,8 +15,7 @@ function draw_game()
     0x0)
   printp_color(47, 0, 0, 0)
 
-  -- show game area
-  rect(0,0,GAME_WIDTH-1,GAME_HEIGHT-1, 35)
+  
   
 
   if gameState == GAME_STATE.SPLASH then
@@ -26,10 +25,22 @@ function draw_game()
     -- todo: title screen
     --draw_level()
 
-  else
+  elseif gameState == GAME_STATE.LVL_PLAY then
     -- normal play (level intro/outro/game-over)    
     --draw_level()
+
+  else
+    -- ??
   end
 
+
+  circfill(0,0,40,3)
+
+  print("HELLO WORLD!",GAME_WIDTH/4,GAME_HEIGHT/2, 6)
+  
+  if DEBUG_MODE then
+    -- show game area
+    rect(0,0,GAME_WIDTH-1,GAME_HEIGHT-1, 35)
+  end
 
 end
