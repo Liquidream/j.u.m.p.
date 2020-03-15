c04d024b4f716cba3034a1868000e7aed5c36748
Paul Nicholas <liquidream@hotmail.co.uk>
2020-03-15 19:11:31 +0000

    Fixed lives count

--- init.lua
+++ init.lua
@@ -458,7 +458,7 @@ end
 -- (will be positioned later)
 function init_blob()
   blob = {
-    lives = 1,
+    lives = 3,
     score = 0,       -- essentially the platform num?
     levelNum = 0,    -- ...this gets set in init_section()
     speedFactor = 1, -- will increase (up to 2.5?) as game progresses
